9d0f7339ec9aab5cf3aafd80438f4695b5a3a4ca
vitor-pf <vitor.dev@hotmail.com>
2022-02-04 14:45:09 -0300

    Criação dos controllers

---
+++ src/main/java/com/nttdata/estacionamento/controllers/CarController.java
@@ -0,0 +1,31 @@
+package com.nttdata.estacionamento.controllers;
+import com.nttdata.estacionamento.entities.CarEntity;
+import com.nttdata.estacionamento.entities.ParkingEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
+@RestController
+@RequestMapping(value = "cars")
+public class CarController {
+    @GetMapping
+    public ResponseEntity<?> findAll(){
+        return ResponseEntity.status(HttpStatus.OK).body("ok");
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById(@PathVariable @Valid Long id){
+        return ResponseEntity.status(HttpStatus.OK).body("id ok");
+    }
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody @Valid CarEntity entity){
+        return ResponseEntity.status(HttpStatus.CREATED).body("save");
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable @Valid Long id, @RequestBody @Valid CarEntity entity){
+        return ResponseEntity.status(HttpStatus.OK).body("update");
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable @Valid Long id){
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+}
