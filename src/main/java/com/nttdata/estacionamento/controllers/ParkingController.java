01eb862da0d42aecf54472e59e97194346df8614
vitor-pf <vitor.dev@hotmail.com>
2022-02-07 15:36:33 -0300

    Ajuste nos services, uso das interfaces

--- src/main/java/com/nttdata/estacionamento/controllers/ParkingController.java
+++ src/main/java/com/nttdata/estacionamento/controllers/ParkingController.java
@@ -2,36 +2,39 @@ package com.nttdata.estacionamento.controllers;
 import com.nttdata.estacionamento.entities.ParkingEntity;
 import com.nttdata.estacionamento.repositories.CarRepository;
 import com.nttdata.estacionamento.repositories.ParkingRepository;
+import com.nttdata.estacionamento.services.ParkingInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/estacionamentos")
 public class ParkingController {
     @Autowired
-    ParkingRepository repository;
+    ParkingInterface parkingInterface;
 
     @GetMapping
-    public ResponseEntity<?> findAll(){
-        return ResponseEntity.status(HttpStatus.OK).body(repository.findAll());
+    public ResponseEntity<List<ParkingEntity>> findAll(){
+        return ResponseEntity.status(HttpStatus.OK).body(parkingInterface.findAll());
     }
     @GetMapping("/{id}")
-    public ResponseEntity<?> findById(@PathVariable @Valid Long id){
-        return ResponseEntity.status(HttpStatus.OK).body(repository.findById(id));
+    public ResponseEntity<ParkingEntity> findById(@PathVariable @Valid Long id){
+        return ResponseEntity.status(HttpStatus.OK).body(parkingInterface.findById(id));
     }
     @PostMapping
-    public ResponseEntity<?> save(@RequestBody @Valid ParkingEntity entity){
-        return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(entity));
+    public ResponseEntity<ParkingEntity> save(@RequestBody @Valid ParkingEntity entity){
+        return ResponseEntity.status(HttpStatus.CREATED).body(parkingInterface.save(entity));
     }
     @PutMapping("/{id}")
-    public ResponseEntity<?> update(@PathVariable @Valid Long id, @RequestBody @Valid ParkingEntity entity){
-        return ResponseEntity.status(HttpStatus.OK).body("update");
+    public ResponseEntity<ParkingEntity> update(@PathVariable @Valid Long id, @RequestBody @Valid ParkingEntity entity){
+        return ResponseEntity.status(HttpStatus.OK).body(parkingInterface.update(id,entity));
     }
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> delete(@PathVariable @Valid Long id){
-        repository.deleteById(id);
+    public ResponseEntity delete(@PathVariable @Valid Long id){
+        parkingInterface.delete(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
--- src/main/java/com/nttdata/estacionamento/controllers/UtilityController.java
+++ src/main/java/com/nttdata/estacionamento/controllers/UtilityController.java
@@ -2,36 +2,39 @@ package com.nttdata.estacionamento.controllers;
 import com.nttdata.estacionamento.entities.UtilityEntity;
 import com.nttdata.estacionamento.repositories.CarRepository;
 import com.nttdata.estacionamento.repositories.UtilityRepository;
+import com.nttdata.estacionamento.services.UtilityInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/utilitarios")
 public class UtilityController {
     @Autowired
-    UtilityRepository repository;
+    UtilityInterface utilityInterface;
 
     @GetMapping
-    public ResponseEntity<?> findAll(){
-        return ResponseEntity.status(HttpStatus.OK).body(repository.findAll());
+    public ResponseEntity<List<UtilityEntity>> findAll(){
+        return ResponseEntity.status(HttpStatus.OK).body(utilityInterface.findAll());
     }
     @GetMapping("/{placa}")
-    public ResponseEntity<?> findById(@PathVariable @Valid String placa){
-        return ResponseEntity.status(HttpStatus.OK).body(repository.findById(placa));
+    public ResponseEntity<UtilityEntity> findById(@PathVariable @Valid String placa){
+        return ResponseEntity.status(HttpStatus.OK).body(utilityInterface.findById(placa));
     }
     @PostMapping
-    public ResponseEntity<?> save(@RequestBody @Valid UtilityEntity entity){
-        return ResponseEntity.status(HttpStatus.CREATED).body("save");
+    public ResponseEntity<UtilityEntity> save(@RequestBody @Valid UtilityEntity entity){
+        return ResponseEntity.status(HttpStatus.CREATED).body(utilityInterface.save(entity));
     }
     @PutMapping("/{placa}")
-    public ResponseEntity<?> update(@PathVariable @Valid Long placa, @RequestBody @Valid UtilityEntity entity){
-        return ResponseEntity.status(HttpStatus.OK).body("update");
+    public ResponseEntity<UtilityEntity> update(@PathVariable @Valid String placa, @RequestBody @Valid UtilityEntity entity){
+        return ResponseEntity.status(HttpStatus.OK).body(utilityInterface.update(placa, entity));
     }
     @DeleteMapping("/{placa}")
-    public ResponseEntity<?> delete(@PathVariable @Valid String placa){
-        repository.deleteById(placa);
+    public ResponseEntity delete(@PathVariable @Valid String placa){
+        utilityInterface.delete(placa);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
